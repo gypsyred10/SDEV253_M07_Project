826d2059fc2d9f286fd7d4a61c512a7d46104840
gypsyred10 <82721123+gypsyred10@users.noreply.github.com>
2021-10-04 17:49:12 -0400

    Add files via upload

---
+++ data_valid_fns.php
@@ -0,0 +1,23 @@
+<?php
+function filled_out($form_vars) {
+	// test that each variable has a value
+    foreach ($form_vars as $key => $value) {
+        if ((!isset($key)) || ($value == '')) {
+            return false;
+        }
+    }
+	return true;
+}
+?>
+
+<?php
+function valid_email($address) {
+	// check an email address is possibly valid
+    if (preg_match('/^[a-zA-z0-9_\.\-]+@[a-zA-z0-9\-]+\.[a-zA-z0-9\-\.]+$/',
+$address))
+    {
+        return true;
+    } else {
+        return false;
+    }
+}
---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+
+ //require_once('bookmark_fns.php');
+
+ // ▼ this line is needed for the HTML to Load 
+ require_once('output_fns.php');
+
+
+ do_html_header('');
+
+ display_site_info();
+ display_login_form();
+
+ do_html_footer();
+?> 
